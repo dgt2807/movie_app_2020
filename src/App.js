5b4e0d29acdb94330c374b85449947f64952ee06
unknown <arcadiavan138182@gmail.com>
2020-09-30 03:47:29 +0900

    Edit App.js 04장 p100

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from 'react';
+import img01 from './images/1.jpg';
+import img02 from './images/2.jpg';
+import img03 from './images/3.jpg';
+import img04 from './images/4.jpg';
+import img05 from './images/5.jpg';
 
-function Food({mem}){
-return <h1>way point {mem }</h1>;
+
+function Food({name,picture,mia}){
+return (
+<div>
+<h1>I love {name}</h1>;
+<img src={picture} alt= {mia}/>
+ </div> 
+ );
 }
 
+const LikeWhat =[
+  {id: 1,name:'no 1',image:img01,mia:'no 1'},
+  {id: 2,name:'no 2',image:img02,mia:'no 2'},
+  {id: 3,name:'no 3',image:img03,mia:'no 3'},
+  {id: 4,name:'no 4',image:img04,mia:'no 4'},
+  {id: 5,name:'no 5',image:img05,mia:'no 5'},
+];
 function App() {
     return (
       <div>
-        <Food mem="vegas"  />
-        <Food mem="york"  />
-        <Food mem="rigon"  />
-        <Food mem="pheonix"  />
-        <Food mem="georgia"  />
-
+        {LikeWhat.map(dish =>(<Food key={dish.id} name={dish.name} picture={dish.image} mia={dish.mia}/>))}
       </div>
       );
     }
