46a86e9b10d2750a27a0c83db1382aba1960d846
unknown <arcadiavan138182@gmail.com>
2020-10-07 11:02:08 +0900

    add 10/7

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import img01 from './images/1.jpg';
 import img02 from './images/2.jpg';
 import img03 from './images/3.jpg';
@@ -6,10 +7,11 @@ import img04 from './images/4.jpg';
 import img05 from './images/5.jpg';
 
 
-function Food({name,picture,mia}){
+function Food({name,picture,mia,rating}){
 return (
 <div>
 <h1>I love {name}</h1>;
+<h4>{rating}/5.0</h4>
 <img src={picture} alt= {mia}/>
  </div> 
  );
@@ -25,9 +27,19 @@ const LikeWhat =[
 function App() {
     return (
       <div>
-        {LikeWhat.map(dish =>(<Food key={dish.id} name={dish.name} picture={dish.image} mia={dish.mia}/>))}
+        {LikeWhat.map(dish =>
+          (<Food key={dish.id} name={dish.name} picture={dish.image} mia={dish.mia} rating={dish.rating}/>
+          ))}
       </div>
       );
     }
 
+
+Food.propTypes = { 
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.ispRequired,
+  mia: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+
+}    
 export default App;
